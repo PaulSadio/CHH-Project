7fbcfeab82f806468ff644e0e2108311abf858ed
PaulSadio <paul.chris.sadio@gmail.com>
2023-11-11 11:04:44 +0800

    Sign In page

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign In</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="sign">
+    <div class="card bg-white text-center ">
+        <nav class="navbar my-3">
+            <div class="container nav">
+                <img src="CHHLogo.png" alt="CHH Logo">
+                <h1 class="display-2 mx-2">CHH</h1>
+                <h5><strong>C</strong>ommunity <br> 
+                    <strong>H</strong>elping <br> 
+                    <strong>H</strong>and</h5>
+            </div>
+        </nav>
+        <div class="card-body mb-3">
+            <h1 class="mb-4">Sign In</h1>
+            <div class="container my-3">
+                <label for="id">Enter ID:</label>
+                <div>
+                <input type="text" name="id" class="id">
+                </div>
+            </div>
+            <div class="container my-3">
+                <label for="passowrd">Password:</label>
+                <div>
+                <input type="text" name="password" class="password">
+                </div>
+            </div>
+            <input type="button" value="Login" class="btn">
+        </div>
+    </div>    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+</body>
+</html>
